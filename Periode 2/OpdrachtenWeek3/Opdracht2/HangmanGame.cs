7a99f7c8390ddf5cd3d6b8945427b3120730a3ae
Project OS <Project OS@DESKTOP-84LC36B>
2018-11-27 23:06:05 +0100

    Started Opdracht2
    tot nu toe niks moeilijks

---
+++ Periode 2/OpdrachtenWeek3/Opdracht2/HangmanGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht2
+{
+    class HangmanGame
+    {
+        public string secretWord, guessedWord;
+
+        public void Init(string secretword)
+        {
+            secretWord = secretword;
+            guessedWord = "";
+            for (int i = 0; i < secretword.Length; i++)
+            {
+                guessedWord = guessedWord + ".";
+            }
+        }
+    }
+}
---
+++ Periode 2/OpdrachtenWeek3/Opdracht2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            List<string> words = new List<string>();
+            HangmanGame hangman = new HangmanGame();
+            hangman.Init("backdoor");
+            Console.WriteLine("The secret word is: " + hangman.secretWord);
+            Console.WriteLine("The guessed word is: " + hangman.guessedWord);
+            Console.ReadKey();
+        }
+
+
+        List<string> ListOfWords()
+        {
+
+            return 
+        }
+
+        string SelectWord(List<string> words)
+        {
+
+        }
+    }
+}
