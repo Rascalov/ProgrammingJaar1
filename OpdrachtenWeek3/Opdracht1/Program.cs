d131703787af35bd15c41e1d6c53be880f4d7c28
Project OS <Project OS@DESKTOP-84LC36B>
2018-11-27 12:12:59 +0100

    Update Program.cs

--- OpdrachtenWeek3/Opdracht1/Program.cs
+++ OpdrachtenWeek3/Opdracht1/Program.cs
@@ -6,7 +6,7 @@ namespace Opdracht1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("!");
+            Console.WriteLine("Do you read me dude?");
         }
     }
 }
