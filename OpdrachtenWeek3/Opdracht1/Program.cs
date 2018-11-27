e0ab6cc1167ce48e2294d2809dbf0db003cc48a7
Project OS <Project OS@DESKTOP-84LC36B>
2018-11-27 12:03:27 +0100

    Update Program.cs

--- OpdrachtenWeek3/Opdracht1/Program.cs
+++ OpdrachtenWeek3/Opdracht1/Program.cs
@@ -6,7 +6,7 @@ namespace Opdracht1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!eeeeee");
+            Console.WriteLine("!");
         }
     }
 }
