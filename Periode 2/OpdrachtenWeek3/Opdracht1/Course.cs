533bdda14e532fecc585c5cf60ca21dd84f7233d
не знаю <31393063+Rascalov@users.noreply.github.com>
2018-11-27 19:43:49 +0100

    late
    changes

---
+++ Periode 2/OpdrachtenWeek3/Opdracht1/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht1
+{
+    class Course
+    {
+        public string Name;
+        public int Grade;
+        public PracticalGrade pGrade;
+
+    }
+    public enum PracticalGrade
+    {
+        None, Absent, Insufficient, Sufficient, Good
+    }
+}
--- Periode 2/OpdrachtenWeek3/Opdracht1/Program.cs
+++ Periode 2/OpdrachtenWeek3/Opdracht1/Program.cs
@@ -6,7 +6,83 @@ namespace Opdracht1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            PracticalGrade pGrade;
+
+            Console.WriteLine("Enter a course.");
+            ReadCourse("Name of course");
+            ReadPracticalGrade("Practical grade for Programming 1: ");
+            DisplayCourse
+
+        }
+
+        PracticalGrade ReadPracticalGrade(string question)
+        {
+            // lijst van opties
+            for (int i = 0; i < 6; i++)
+            {
+                Console.Write(i + ".  " + (PracticalGrade)1 + "  ");
+            }
+            Console.WriteLine();
+            PracticalGrade pGrade = (PracticalGrade)LeesInt(question);
+            return pGrade;
+        }
+        void DisplayPracticalGrade(PracticalGrade practical)
+        {
+            Console.Write(practical);
+        }
+
+        Course ReadCourse(string question)
+        {
+            Course vak = new Course();
+            
+            vak.Name = LeesString(question);
+            return vak;
+        }
+        void DisplayCourse(Course course)
+        {
+            Console.Write(course.Name);
+        }
+
+
+
+
+
+
+
+
+
+
+        // Deze methode leest een integer en geeft die waarde terug.
+        int LeesInt(string vraag)
+        {
+            int invoer;
+            Console.Write(vraag);
+            invoer = int.Parse(Console.ReadLine());
+            return invoer;
+        }
+        // Deze methode krijgt een invoer binnen en als het tussen de gegeven waardes ligt gaat het programma door, zo niet loopt hij. 
+        int LeesInt(string vraag, int min, int max)
+        {
+            int invoer;
+
+            do
+            {
+                invoer = LeesInt(vraag);
+            } while (invoer > max || invoer < min);
+            return invoer;
+        }
+        // Deze methode leest een string in.
+        string LeesString(string vraag)
+        {
+            string invoer;
+            Console.Write(vraag);
+            invoer = Console.ReadLine();
+            return invoer;
         }
     }
 }
