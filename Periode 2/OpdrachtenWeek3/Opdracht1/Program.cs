8ffce726489d3c3b4595fa6ccff5cbf75d662a1a
Project OS <Project OS@DESKTOP-84LC36B>
2018-11-27 22:27:07 +0100

    Opdracht 1 af
    Geen specifieke moeilijkheden gevonden, simpelweg lezen en doen.

--- Periode 2/OpdrachtenWeek3/Opdracht1/Program.cs
+++ Periode 2/OpdrachtenWeek3/Opdracht1/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Opdracht1
 {
     class Program
     {
+        int teller = 0;
         static void Main(string[] args)
         {
             Program myProgram = new Program();
@@ -11,21 +13,62 @@ namespace Opdracht1
         }
         void Start()
         {
-            PracticalGrade pGrade;
+            List<Course> report = new List<Course>();
+            
+            Course vak = new Course();
 
-            Console.WriteLine("Enter a course.");
-            ReadCourse("Name of course");
-            ReadPracticalGrade("Practical grade for Programming 1: ");
-            DisplayCourse
+            DisplayReport(ReadReport(report));
+
+
+            Console.ReadKey();
+          
 
         }
+
+        List<Course> ReadReport(List<Course> list)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter a course.");
+                list.Add(ReadCourse("Name of the course: "));
+                Console.WriteLine();
+            }
+            
+            return list;
+        }
+
+        void DisplayReport(List<Course> list)
+        {
+            foreach(Course vak in list)
+            {
+                string result = String.Format("{0,-15} : {1}\t {2}", vak.Name, vak.Grade, vak.pGrade);
+                Console.WriteLine(result);
+            }
+            if (Passed(list))
+            {
+                if (CumLaude(list))
+                {
+                    Console.WriteLine("Amazing, you graduated cum laude! With (of course) {0} retakes needed", teller);
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations, you graduated! {0} retakes needed!", teller);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Too bad, you did not graduate, you got {0} retake(s).", teller);
+            }
+        }
+
+
 
         PracticalGrade ReadPracticalGrade(string question)
         {
             // lijst van opties
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 5; i++)
             {
-                Console.Write(i + ".  " + (PracticalGrade)1 + "  ");
+                Console.Write(i + ".  " + (PracticalGrade)i + "  ");
             }
             Console.WriteLine();
             PracticalGrade pGrade = (PracticalGrade)LeesInt(question);
@@ -41,14 +84,51 @@ namespace Opdracht1
             Course vak = new Course();
             
             vak.Name = LeesString(question);
+            vak.Grade = LeesInt("Grade for " + vak.Name + ": ");
+            vak.pGrade = ReadPracticalGrade("Practical grade for " + vak.Name + ": ");
+
             return vak;
         }
         void DisplayCourse(Course course)
         {
             Console.Write(course.Name);
         }
+        public bool Passed(List<Course> vakken)
+        {
+            
+            foreach(Course vak in vakken)
+            {
+                if (vak.Grade < 55)
+                {
+                    teller++;
+                }
+                if ((int)vak.pGrade < 3)
+                {
+                    teller++;
+                }
+            }
+            if (teller != 0)
+            {
+                return false;
+            }
 
 
+            return true;
+        }
+
+        public bool CumLaude(List<Course> vakken)
+        {
+            foreach (Course vak in vakken)
+            {
+                if (vak.Grade < 80)
+                {
+                    return false;
+                }
+            }
+          
+
+            return true;
+        }
 
 
 
@@ -62,7 +142,9 @@ namespace Opdracht1
         {
             int invoer;
             Console.Write(vraag);
+            Console.ForegroundColor = ConsoleColor.Green;
             invoer = int.Parse(Console.ReadLine());
+            Console.ResetColor();
             return invoer;
         }
         // Deze methode krijgt een invoer binnen en als het tussen de gegeven waardes ligt gaat het programma door, zo niet loopt hij. 
@@ -81,7 +163,9 @@ namespace Opdracht1
         {
             string invoer;
             Console.Write(vraag);
+            Console.ForegroundColor = ConsoleColor.Green;
             invoer = Console.ReadLine();
+            Console.ResetColor();
             return invoer;
         }
     }
