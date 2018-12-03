a2b2963d1e76d1605b41c575f692fffa30f5f816
Project OS <Project OS@DESKTOP-84LC36B>
2018-12-03 21:58:32 +0100

    Opdracht2 van week 3 copied naar 4 + words.txt

---
+++ Periode 2/OpdrachtenWeek4/Opdracht2Redo/GalgjeSpel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Opdracht2Redo
+{
+    class GalgjeSpel
+    {
+        public string geheimWoord, geradenWoord;
+
+        public void Init(string geheimwoord)
+        {
+            geheimWoord = geheimwoord;
+            geradenWoord = "";
+            for (int i = 0; i < geheimwoord.Length; i++)
+            {
+                geradenWoord = geradenWoord + ".";
+            }
+        }
+        public bool RaadLetter(char letter)
+        {
+            if (geheimWoord.Contains(letter))
+            {
+                // pleur geradenwoord value naar een char array
+                char[] deLetters = geradenWoord.ToCharArray();
+                for (int i = 0; i < geheimWoord.Length; i++)
+                {
+                    if(letter == geheimWoord[i])
+                    {
+                       // verander de char array 
+                       deLetters[i] = letter;
+                    }
+                }
+                // zet char array weer om naar een string
+                String letters = new string(deLetters);
+                // en zet het woord weer gelijk aan die string
+                geradenWoord = letters;
+
+                return true;
+            }
+
+            return false;
+        }
+        public bool IsGeraden()
+        {
+
+            if (geradenWoord == geheimWoord)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
