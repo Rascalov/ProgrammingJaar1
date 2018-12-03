7ffe790a5c588a40745d005c9c05464b5fc4b920
Project OS <Project OS@DESKTOP-84LC36B>
2018-12-03 23:25:15 +0100

    Added methods
    fuck you

--- Periode 2/OpdrachtenWeek4/Opdracht3/Program.cs
+++ Periode 2/OpdrachtenWeek4/Opdracht3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Opdracht3
 {
@@ -6,7 +7,44 @@ namespace Opdracht3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            Console.Write("Zoek woord: ");
+            string woord = Console.ReadLine();
+
+            Console.WriteLine(ZoekWoordInBestand("Tweets.txt", woord));
+            Console.ReadKey();
+        }
+
+        bool ZitWoordInRegel(string regel, string woord)
+        {
+            if (regel.ToLower().Contains(woord.ToLower())){
+                return true;
+            }
+
+            return false;
+        }
+        int ZoekWoordInBestand(string bestandsnaam, string woord)
+        {
+            int teller = 0;
+            StreamReader reader = new StreamReader(bestandsnaam);
+            while (!reader.EndOfStream)
+            {
+                if (ZitWoordInRegel(reader.ReadLine(), woord))
+                {
+                    teller++;
+                }
+            }
+            return teller;
+        }
+
+        void ToonWoordInRegel(string regel, string woord)
+        {
+            
+            Console.WriteLine(regel);
         }
     }
 }
