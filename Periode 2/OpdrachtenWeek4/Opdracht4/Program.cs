d3d19f78f241818094ebb64efa13dfbe09d318bb
Project OS <Project OS@DESKTOP-84LC36B>
2018-12-05 11:38:41 +0100

    Update Program.cs

--- Periode 2/OpdrachtenWeek4/Opdracht4/Program.cs
+++ Periode 2/OpdrachtenWeek4/Opdracht4/Program.cs
@@ -143,9 +143,6 @@ namespace Opdracht3
                     }
                 }
             }
-            
-
-
             return false;
         }
 
@@ -181,10 +178,6 @@ namespace Opdracht3
                     }
                 }
             }
-
-
-
-
             return false;
         }
 
