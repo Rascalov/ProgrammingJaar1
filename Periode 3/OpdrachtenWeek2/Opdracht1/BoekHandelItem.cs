c4157b22baf46f2c82b9aec7835c1e53da52d58d
Project OS <Project OS@DESKTOP-84LC36B>
2019-02-26 13:32:43 +0100

    added wat spul
    is niets belangrijks

---
+++ Periode 3/OpdrachtenWeek2/Opdracht1/BoekHandelItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht1
+{
+    abstract class BoekHandelItem
+    {
+        public string titel;
+        public double prijs;
+        public virtual void PrintItemInfo()
+        {
+
+        }
+    }
+}
---
+++ Periode 3/OpdrachtenWeek3/Opdracht1/BoekHandelItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht1
+{
+    abstract class BoekHandelItem
+    {
+        private string titel;
+        public string Titel
+        {
+            get { return titel; }
+            set { titel = value; }
+        }
+        public double prijs;
+        public virtual void PrintItemInfo()
+        {
+
+        }
+    }
+}
